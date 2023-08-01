bf43023f8d11330df8882e3fdaa387374fbd8b46
Jazlin Yu <jazlinsub@gmail.com>
2023-08-01 15:40:01 -0700

    passing test

---
+++ index.js
@@ -0,0 +1,15 @@
+/** @format */
+
+const divide = function (a, b) {
+  return a / b;
+};
+
+console.log(divide(2000, 100));
+
+const square = (number) => {
+  return number * number;
+};
+
+const add = (a, b) => {
+  return a + b;
+};
